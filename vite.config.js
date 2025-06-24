b0af1401d8ff507440363d46810530b81d31b85d
Ankit <iankit.chourasia@gmail.com>
2025-06-24 11:58:46 +0530

    Trial

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+// vite.config.js
+export default {
+  base: '/Food-delivery/', // <- this is IMPORTANT
+};
+
