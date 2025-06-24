756598489262f7cb8c83ca36c54bf9515fd73fa2
Ankit <iankit.chourasia@gmail.com>
2025-06-24 13:22:27 +0530

    last

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,9 +5,9 @@ const Header = () => {
     return (
         <div className='header'>
             <div className="header-contents">
-                <h1>Order your food here</h1>
+                <h2>Order your food here</h2>
                 <p>Choose from a diverse menu featuring a detectable array of disher crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience , one delicious meal at a time.</p>
-                <button>View Menu</button>
+                <button>View Menu.</button>
             </div>
         </div>
     )
